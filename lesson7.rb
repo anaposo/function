f882d18fd2259d5e8679e5e4a3a0977dca0e1b50
anaposo <ana.barbu.19@gmail.com>
2014-04-22 22:04:27 -0400

    lesson7 commit

---
+++ lesson7.rb
@@ -0,0 +1,37 @@
+#add five to argument
+def addFive (n)
+	n + 5
+end 
+
+a = addFive(7)
+	puts a 
+
+#multiply argument by 15
+def multiply(b)
+	puts b * 15
+end 
+
+m = multiply(7)
+	puts m 
+
+#performa a math operation with integers and floats
+x=4
+	if x<2 
+		puts x * 0.15
+	elsif x>2 
+		puts x * 0.30
+end
+
+#prints the argument given four times
+
+4.times do
+  puts "I am the printed argument"
+end 
+
+#prints an uppercase version two times
+2.times do 
+	puts "tea ".upcase + " time".upcase
+end
+
+
+
